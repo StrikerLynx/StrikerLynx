168ed38bc8298c70550a0be39bf4c8e1111442da
StrikerLynx <Silentrecon56@gmail.com>
2026-08-21 03:15:08 -0400

    Added Emacs Config

---
+++ .config/emacs/init.el
@@ -0,0 +1,46 @@
+;------------------;
+;   Emacs Config   ;
+;------------------;
+(tool-bar-mode -1)
+(global-display-line-numbers-mode 1)
+
+
+
+
+
+(setq mouse-yank-at-point t)
+
+
+;; Allow for shorter responses: "y" for yes and "n" for no.
+(setq read-answer-short t)
+(if (boundp 'use-short-answers)
+    (setq use-short-answers t)
+  (advice-add 'yes-or-no-p :override #'y-or-n-p))
+(setq revert-buffer-quick-short-answers t)
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+(package-refresh-contents)
+
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+
+(setq evil-undo-system 'undo-redo)
+
+(require 'evil)
+(evil-mode 1)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
