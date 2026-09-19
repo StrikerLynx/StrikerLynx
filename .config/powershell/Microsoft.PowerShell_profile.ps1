7e7420283a042cc5c50ef9ca1a6757dd3bf1f41e
StrikerLynx <Silentrecon56@gmail.com>
2026-09-19 01:08:02 -0400

    Updated PwSh Config

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++
@@ -1,4 +0,0 @@
-# PowerShell Config
-$env:XDG_CONFIG_HOME = "$HOME\.config"
-$env:MPV_HOME = "$HOME\.config\mpv"
-$env:DOTNET_CLI_TELEMETRY_OPTOUT =  "1"
--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -8,3 +8,14 @@ $dwn = "$env:USERPROFILE\Downloads"
 $dt = "$env:USERPROFILE\Desktop"
 $doc = "$env:USERPROFILE\Documents"
 $tools = "D:\Tools"
+
+# Program Aliases
+Set-Alias np "C:\Program Files\Notepad++\notepad++.exe"
+Set-Alias ed edit
+Set-Alias make mingw32-make
+Set-Alias note notepad
+Set-Alias vi vim
+
+# Program Functions
+function Edge_Run {Start-Process msedge}
+function qqq {exit}
